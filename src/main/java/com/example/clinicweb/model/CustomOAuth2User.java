8ea9b80f993676884b96bdf7b6f6e44e72079439
tungd <tungduong2288@gmail.com>
2024-11-22 12:38:09 +0700

    Đăng nhập bằng google

---
+++ src/main/java/com/example/clinicweb/model/CustomOAuth2User.java
@@ -0,0 +1,68 @@
+package com.example.clinicweb.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User, UserDetails {
+
+    private final OAuth2User oAuth2User;
+    private final String username;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public CustomOAuth2User(OAuth2User oAuth2User, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.oAuth2User = oAuth2User;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2User.getAttributes();
+    }
+
+    @Override
+    public String getName() {
+        return oAuth2User.getName();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
