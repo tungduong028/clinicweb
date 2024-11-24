5e69a3efa8ab942c553a4521eeb5107f6d7bc218
tungd <tungduong2288@gmail.com>
2024-11-24 15:58:36 +0700

    test cloud storage

---
+++ src/main/java/com/example/clinicweb/service/CloudStorageService.java
@@ -0,0 +1,33 @@
+package com.example.clinicweb.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Service
+public class CloudStorageService {
+
+    private final Storage storage;
+
+    public CloudStorageService() throws IOException {
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream("C:/elevated-axon-442412-c5-55a42ddda5c6.json"));
+        storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String bucketName = "clinicweb";
+        String blobName = file.getOriginalFilename();
+
+        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, blobName).build();
+        storage.create(blobInfo, file.getBytes());
+
+        return String.format("https://storage.googleapis.com/%s/%s", bucketName, blobName);
+    }
+}
+
